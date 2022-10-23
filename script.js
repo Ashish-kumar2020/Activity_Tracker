afefa1942e02f59a7b9bb7619a7c315dee26c26c
Ashish Kumar Singh <ashishsinghk2020@gmail.com>
2022-10-23 11:11:25 +0530

    Rendering workout form

--- script.js
+++ script.js
@@ -1,54 +1,76 @@
-'use strict';
+"use strict";
 
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const form = document.querySelector('.form');
-const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
+const form = document.querySelector(".form");
+const containerWorkouts = document.querySelector(".workouts");
+const inputType = document.querySelector(".form__input--type");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputCadence = document.querySelector(".form__input--cadence");
+const inputElevation = document.querySelector(".form__input--elevation");
 
 
+let map,mapEvent;
 // adding geolocation api
 // it takes two parameter success and failed
 if (navigator.geolocation)
-    navigator.geolocation.getCurrentPosition
-        (function (position) {
-            // success
-            console.log(position);
-            const { latitude } = position.coords;
-            const { longitude } = position.coords;
-            console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      // success
+      console.log(position);
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
 
-            const coords = [latitude,longitude]
-            // displaying a map using leaflet library
-            const map = L.map('map').setView(coords, 13);
+      const coords = [latitude, longitude];
+      // displaying a map using leaflet library
+      map = L.map("map").setView(coords, 13);
 
-            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            }).addTo(map);
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
 
-           
-            
-            map.on('click',function(mapEvent){
-                const {lat,lng} = mapEvent.latlng;
-                L.marker([lat,lng]).addTo(map)
-                .bindPopup(L.popup({
-                    maxWidth: 250,
-                    minWidth: 100,
-                    autoClose: false,
-                    closeOnClick: false,
-                    className: 'running-popup',
-                    
-                }))
-                .setPopupContent('Workout')
-                .openPopup();
-            });
-        }, function () {
-            // failed
-            alert('Could not get your location')
-        })
+      // handling click on map
+      map.on("click", function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove("hidden");
+        inputDistance.focus();
+      });
+    },
+    function () {
+      // failed
+      alert("Could not get your location");
+    }
+  );
 
+form.addEventListener("submit", function (e) {
+  // display the marker
+  e.preventDefault();
+
+  // clear input fields
+  inputDistance.value = inputCadence.value = inputDuration.value = inputElevation.value = "";
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: "running-popup",
+      })
+    )
+    .setPopupContent("Workout")
+    .openPopup();
+});
+
+
+// handle input type change from cycling to workout
+inputType.addEventListener('change',function(){
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+})
