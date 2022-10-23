e4629dc417cb106651cdb579a2b9afdc2915f79e
Ashish Kumar Singh <ashishsinghk2020@gmail.com>
2022-10-23 10:21:37 +0530

    added leafty map

--- script.js
+++ script.js
@@ -14,15 +14,28 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 // adding geolocation api
 // it takes two parameter success and failed
-if(navigator.geolocation)
+if (navigator.geolocation)
     navigator.geolocation.getCurrentPosition
-    (function(position){
-        // success
-        console.log(position);
-        const {latitude} = position.coords;
-        const {longitude} = position.coords;
-        console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
-    },function(){
-    // failed
-    alert('Could not get your location')
-})
+        (function (position) {
+            // success
+            console.log(position);
+            const { latitude } = position.coords;
+            const { longitude } = position.coords;
+            console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+
+            const coords = [latitude,longitude]
+            // displaying a map using leaflet library
+            const map = L.map('map').setView(coords, 13);
+
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+
+            L.marker(coords).addTo(map)
+                .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+                .openPopup();
+        }, function () {
+            // failed
+            alert('Could not get your location')
+        })
+
